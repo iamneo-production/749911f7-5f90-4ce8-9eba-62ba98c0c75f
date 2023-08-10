581d6753c008a26b318b554089971aecdcfbe806
vmulaga <tlv.mulaga@gmail.com>
2023-08-10 22:23:05 +0000

    Task: Prettier, React-Router configured and Post component created

---
+++ hackathon-blog-webapp/src/Components/Post/Post.js
@@ -0,0 +1,24 @@
+function Post() {
+  return (
+    <div className="post">
+      <img
+        src="https://img.freepik.com/free-vector/hackathon-technology-infographic-with-flat-icons_88138-961.jpg?w=2000"
+        width="500"
+        height="300"
+        alt="image"
+      />
+      <h2>Welcome to React JS Hackathon</h2>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </p>
+    </div>
+  );
+}
+
+export default Post;
